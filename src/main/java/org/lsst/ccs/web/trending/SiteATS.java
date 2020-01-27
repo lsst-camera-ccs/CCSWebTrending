2d28bf892ba620ac99f4918f9a889c1c052fa801
Tony Johnson <tonyj@stanford.edu>
2020-01-27 08:17:34 -0800

    Update ATS data for the summit

--- src/main/java/org/lsst/ccs/web/trending/SiteATS.java
+++ src/main/java/org/lsst/ccs/web/trending/SiteATS.java
@@ -18,10 +18,10 @@ public class SiteATS extends Site {
         Properties props = new Properties();
         props.setProperty("name", "ats");
         props.setProperty("useSSH", "true");
-        props.setProperty("restURL", "http://atsccs1.lsst.org:8080/rest/data/dataserver/");
+        props.setProperty("restURL", "http://atsccs1.cp.lsst.org:8080/rest/data/dataserver/");
 
-        props.setProperty("ssh.user", "ccs");
-        props.setProperty("ssh.host", "140.252.32.130");
+        props.setProperty("ssh.user", "tonyj");
+        props.setProperty("ssh.host", "stargate.lsst.org");
         props.setProperty("ssh.key", "/nfs/slac/g/srs/.ssh/ccs");
         return new SiteATS(props);
     }
